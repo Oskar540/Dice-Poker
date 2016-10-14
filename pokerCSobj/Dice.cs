1279050d90c97af57ca25cb27e39262ea1eb1825
Oskar540 <xmrplayerowyx@gmail.com>
2016-10-14 14:49:05 +0200

    Dice Poker
    first project i've made

---
+++ pokerCSobj/Dice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokerCSobj
+{
+    public class Dice
+    {
+        public int vault;
+        Random rnd = new Random();
+
+        public Dice()
+        {
+            
+            vault = rnd.Next(1, 7);
+            System.Threading.Thread.Sleep(10);
+            Console.Write("[ {0} ]", vault);
+        }
+    }
+}
---
+++ pokerCSobj/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokerCSobj
+{
+    public class Player
+    {
+        public int score, sum;
+        public Dice[] dices;
+
+        public Player()
+        {
+            
+            Console.WriteLine("Gracz nr x");
+            score = 0;
+            dices = new Dice[5];
+            for (int i = 0; i < dices.Length; i++) dices[i] = new Dice();
+            for (int i = 0; i < dices.Length; i++) sum += dices[i].vault;
+            Console.WriteLine("Suma gracza: {0}", sum);
+        } 
+    }
+}
